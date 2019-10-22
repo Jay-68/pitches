284b8d19ab10f78544cfe64e8bc6437fdf40d55e
Jay-68 <kelvinngari1995@gmail.com>
2019-10-22 11:02:11 +0300

    add a log out route

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, redirect, url_for, request, flash
 from . import auth
 from flask_login import login_user, logout_user, login_required
-from .views import RegistrationForm,loginForm
+from .views import RegistrationForm, loginForm
 # from ..models import RegistrationForm, loginForm
 from .. import db
 
@@ -21,21 +21,26 @@ def register():
 
 
 # login form
-@auth.route('/login',methods=['GET','POST'])
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-  '''
-  Function that checks if the form is validated
-  '''
-  login_form=loginForm()
-  if login_form.validate_on_submit():
-    user=User.query.filter_by(email=login_form.email.data).first()
-    if user is not None and user.verify_password(login_form.password.data):
-      login_user(user,login_form.remember.data)
-      return redirect(request.args.get('Next') or url_for('main.index'))
+    '''
+    Function that checks if the form is validated
+    '''
+    login_form = loginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email=login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user, login_form.remember.data)
+            return redirect(request.args.get('Next') or url_for('main.index'))
 
-    flash('Invalid username or password')
+        flash('Invalid username or password')
 
-  title='Pitches Log In'
-  return render_template('auth/login.html',login_form=login_form,title=title)
+    title = 'Pitches Log In'
+    return render_template('auth/login.html', login_form=login_form, title=title)
 
-  
+# log out user form
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
