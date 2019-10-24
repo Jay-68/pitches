f84deb4aeb8905727413d023d25d37780780826b
Jay-68 <kelvinngari1995@gmail.com>
2019-10-24 11:51:05 +0300

    prepare application for deployment to heroku

--- config.py
+++ config.py
@@ -39,8 +39,13 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with general configuration settings
     '''
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = os.environ.get('MAIL_PORT')
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
 
 
 class DevConfig(Config):
@@ -51,9 +56,9 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with general configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitch'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    
 
-    DEBUG = True
 
 
 class TestConfig(Config):
@@ -65,8 +70,9 @@ class TestConfig(Config):
         Config: The parent configuration class with general configuration settings
     '''
 
-    DATABASE_PASS = os.environ.get('DATABASE_PASS')
+
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitches'
+    DEBUG = True
 
 
 config_options = {
