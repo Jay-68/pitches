24beffbc50755879425a80f8447d175b6c78169c
Jay-68 <kelvinngari1995@gmail.com>
2019-10-25 12:10:01 +0300

    deploy

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export MAIL_USERNAME ='kelvinngari1995@gmail.com'
 export  MAIL_PASSWORD ='10october23'
 export SECRET_KEY='12345q'
-SQLALCHEMY_DATABASE_URI='DATABASE_URL'
+# SQLALCHEMY_DATABASE_URI='DATABASE_URL'
 python3.6 manage.py server
